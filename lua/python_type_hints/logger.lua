95b3eabe12ff65e77a3dbee5c8b78164b3511226
ctrltouch <dumidu2023@hotmail.com>
2025-09-01 13:40:56 +0530

    debug: logging

--- lua/python_type_hints/logger.lua
+++ lua/python_type_hints/logger.lua
@@ -1,4 +1,5 @@
 -- lua/python_type_hints/logger.lua
+
 local M = {}
 
 M.enabled = false
@@ -6,21 +7,20 @@ M.enabled = false
 function M.log(msg)
 	if M.enabled then
 		local time = os.date("%H:%M:%S")
-		local line = string.format("[%s] %s", time, msg)
-		vim.schedule(function()
-			vim.api.nvim_echo({ { line, "Comment" } }, false, {})
-		end)
+		local line = string.format("[py-hints] %s %s", time, msg)
+		-- ✅ Use print() to ensure it appears in :messages
+		print(line)
 	end
 end
 
 function M.enable()
 	M.enabled = true
-	-- ✅ ADD THIS DEBUG LINE
-	print("🟢 python-type-hints.nvim: Logger ENABLED")
+	print("[py-hints] 🟢 Logger ENABLED")
 end
 
 function M.disable()
 	M.enabled = false
+	-- Optional: print("[py-hints] 🔴 Logger DISABLED")
 end
 
 return M
